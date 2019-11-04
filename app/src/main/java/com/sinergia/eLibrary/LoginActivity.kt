775bb29e76a90e55bda52b944049d83187e51830
unknown <adrianfergo93@gmail.com>
2019-11-04 20:05:27 +0100

    First release with register functionality

--- app/src/main/java/com/sinergia/eLibrary/LoginActivity.kt
+++ app/src/main/java/com/sinergia/eLibrary/LoginActivity.kt
@@ -3,19 +3,41 @@ package com.sinergia.eLibrary
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var login_btn: Button
 
+    private lateinit var login_user: EditText
+    private lateinit var login_pass: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        //Login Variables
+        login_user = findViewById<EditText>(R.id.login_user)
+        login_pass = findViewById<EditText>(R.id.login_pass)
+
+
+        //Login Button and Function
         login_btn = findViewById<Button>(R.id.login_btn)
 
         login_btn.setOnClickListener(){
+
+            if(login_user.text.isNullOrEmpty()){
+                Toast.makeText(this, "Debe introducir un Nombre de Usuario.", Toast.LENGTH_LONG).show()
+            } else if (login_pass.text.isNullOrEmpty()){
+                Toast.makeText(this, "Debe introducir una Contraseña.", Toast.LENGTH_LONG).show()
+            } else {
+
+            }
+
+
+
             Toast.makeText(this, "No lo intentes más pesadilla, que este botón no va aún.", Toast.LENGTH_LONG).show()
         }
 
--- app/src/main/java/com/sinergia/eLibrary/RegisterActivity.kt
+++ app/src/main/java/com/sinergia/eLibrary/RegisterActivity.kt
@@ -1,23 +1,150 @@
 package com.sinergia.eLibrary
 
+//import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.register_activity.*
 
 class RegisterActivity : AppCompatActivity() {
 
+    //Register Variables References
+    private lateinit var register_name: EditText
+    private lateinit var register_lastname: EditText
+    private lateinit var register_username: EditText
+    private lateinit var register_userpass1: EditText
+    private lateinit var register_userpass2: EditText
+    private lateinit var register_progress_bar: ProgressBar
     private lateinit var register_btn: Button
+
+    private var firstName: String? = null
+    private var lastName: String? = null
+    private var email: String? = null
+    private var password: String? = null
+    private var repeatPassword: String? = null
+    private var progressBar: ProgressBar? = null
+
+    private val TAG = "CreateAccountActivity"
+
+    //Firebase References
+    private var nelsDatabaseReference: DatabaseReference? = null
+    private var nelsDatabase: FirebaseDatabase? = null
+    private var nelsAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
 
+        //Register variables
+        register_name = findViewById<EditText>(R.id.register_name)
+        register_lastname = findViewById<EditText>(R.id.register_lastname)
+        register_username = findViewById<EditText>(R.id.register_username)
+        register_userpass1 = findViewById<EditText>(R.id.register_pass)
+        register_userpass2 = findViewById<EditText>(R.id.register_repeatpass)
+        register_progress_bar = findViewById<ProgressBar>(R.id.register_progress_bar)
+
+
+        //Firebase Variables
+        nelsDatabase = FirebaseDatabase.getInstance()
+        nelsDatabaseReference = nelsDatabase!!.reference!!.child("Users")
+        nelsAuth = FirebaseAuth.getInstance()
+
+
+        //Register Button and function
         register_btn = findViewById<Button>(R.id.register_btn)
 
-        register_btn.setOnClickListener(){
-            Toast.makeText(this,  "ESTO AUN NO VA TOLAI, NO LE DES MAS",  Toast.LENGTH_LONG).show()
+        register_btn.setOnClickListener(){ createNewAcount() }
+
+    }
+
+    //Function to create new Account
+    fun createNewAcount(){
+
+        //Register String Variables
+        firstName = register_name?.text.toString()
+        lastName = register_lastname?.text.toString()
+        email = register_username?.text.toString()
+        password = register_pass?.text.toString()
+        repeatPassword = register_userpass2?.text.toString()
+
+        //register_progress_bar = ProgressBar(this)
+        //register_progress_bar.setProgress(1,  true)
+
+        //Register process
+        if(firstName.isNullOrEmpty() || lastName.isNullOrEmpty() || email.isNullOrEmpty() || password.isNullOrEmpty() || repeatPassword.isNullOrEmpty()){
+            Toast.makeText(this, "Todos los campos son obligatorios, por favor completa el formulario de Nuevo Usuario", Toast.LENGTH_SHORT)
+        } else {
+
+            if(password != repeatPassword){
+                Toast.makeText(this, "Las contraseñas no coinciden, por favor vuelve a escribirlas", Toast.LENGTH_SHORT)
+            } else {
+
+                nelsAuth!!
+                    .createUserWithEmailAndPassword(email!!, password!!)
+                    .addOnCompleteListener(this) {task ->
+
+                            if(task.isSuccessful){
+
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success")
+                                val userId = nelsAuth!!.currentUser!!.uid
+
+                                //Verify Email
+                                verifyEmail()
+
+                                //Update user profile information
+                                val currentUserDb = nelsDatabaseReference!!.child(userId)
+                                currentUserDb.child("Nombre").setValue(firstName)
+                                currentUserDb.child("Apellidos").setValue(lastName)
+                                currentUserDb.child("Email").setValue(lastName)
+                                currentUserDb.child("Contraseña").setValue(lastName)
+                                updateUserInfoAndUI()
+
+                            } else {
+
+                                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                                Toast.makeText(this, "Fallo de autonticación, intentalo más tarde.", Toast.LENGTH_SHORT).show()
+
+                            }
+
+                        }
+
+            }
+
         }
+    }
+
+    //Function to update user info and UI
+    private fun updateUserInfoAndUI() {
+        //start next activity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    //Function to verify New User email
+    private fun verifyEmail() {
+
+        val newUser = nelsAuth!!.currentUser
+
+        newUser!!.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this,
+                        "Verification email sent to " + newUser.getEmail(),
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(this,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 }
