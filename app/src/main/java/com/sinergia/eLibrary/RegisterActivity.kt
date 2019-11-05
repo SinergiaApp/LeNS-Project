08c33193d8051b8d8755c4b097198589aa563a55
unknown <adrianfergo93@gmail.com>
2019-11-05 19:17:08 +0100

    refactor register activity. Still have bugs.

--- app/src/main/java/com/sinergia/eLibrary/RegisterActivity.kt
+++ app/src/main/java/com/sinergia/eLibrary/RegisterActivity.kt
@@ -19,7 +19,7 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var register_username: EditText
     private lateinit var register_userpass1: EditText
     private lateinit var register_userpass2: EditText
-    private lateinit var register_progress_bar: ProgressBar
+    private lateinit var register_progressBar: ProgressBar
     private lateinit var register_btn: Button
 
     private var firstName: String? = null
@@ -27,7 +27,6 @@ class RegisterActivity : AppCompatActivity() {
     private var email: String? = null
     private var password: String? = null
     private var repeatPassword: String? = null
-    private var progressBar: ProgressBar? = null
 
     private val TAG = "CreateAccountActivity"
 
@@ -40,14 +39,27 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
 
-        //Register variables
+        initialize()
+
+    }
+
+    //Function to initialized all variables
+    fun initialize(){
+
+        //UI variables
         register_name = findViewById<EditText>(R.id.register_name)
         register_lastname = findViewById<EditText>(R.id.register_lastname)
         register_username = findViewById<EditText>(R.id.register_username)
         register_userpass1 = findViewById<EditText>(R.id.register_pass)
         register_userpass2 = findViewById<EditText>(R.id.register_repeatpass)
-        register_progress_bar = findViewById<ProgressBar>(R.id.register_progress_bar)
+        register_progressBar = findViewById<ProgressBar>(R.id.register_progressBar)
 
+        //Register Function Variables
+        firstName = register_name?.text.toString()
+        lastName = register_lastname?.text.toString()
+        email = register_username?.text.toString()
+        password = register_pass?.text.toString()
+        repeatPassword = register_userpass2?.text.toString()
 
         //Firebase Variables
         nelsDatabase = FirebaseDatabase.getInstance()
@@ -65,25 +77,17 @@ class RegisterActivity : AppCompatActivity() {
     //Function to create new Account
     fun createNewAcount(){
 
-        //Register String Variables
-        firstName = register_name?.text.toString()
-        lastName = register_lastname?.text.toString()
-        email = register_username?.text.toString()
-        password = register_pass?.text.toString()
-        repeatPassword = register_userpass2?.text.toString()
-
-        //register_progress_bar = ProgressBar(this)
-        //register_progress_bar.setProgress(1,  true)
+        register_progressBar.visibility=ProgressBar.VISIBLE
 
         //Register process
         if(firstName.isNullOrEmpty() || lastName.isNullOrEmpty() || email.isNullOrEmpty() || password.isNullOrEmpty() || repeatPassword.isNullOrEmpty()){
-            Toast.makeText(this, "Todos los campos son obligatorios, por favor completa el formulario de Nuevo Usuario", Toast.LENGTH_SHORT)
+            Toast.makeText(this, "Todos los campos son obligatorios, por favor completa el formulario de Nuevo Usuario", Toast.LENGTH_SHORT).show()
         } else {
 
             if(password != repeatPassword){
-                Toast.makeText(this, "Las contraseñas no coinciden, por favor vuelve a escribirlas", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Las contraseñas no coinciden, por favor vuelve a escribirlas", Toast.LENGTH_SHORT).show()
             } else {
-
+                Toast.makeText(this, FirebaseAuth.getInstance().currentUser.toString(), Toast.LENGTH_LONG).show()
                 nelsAuth!!
                     .createUserWithEmailAndPassword(email!!, password!!)
                     .addOnCompleteListener(this) {task ->
@@ -93,6 +97,7 @@ class RegisterActivity : AppCompatActivity() {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success")
                                 val userId = nelsAuth!!.currentUser!!.uid
+                                Toast.makeText(this, "Creando Usuario, por favor espera...", Toast.LENGTH_SHORT).show()
 
                                 //Verify Email
                                 verifyEmail()
@@ -101,14 +106,18 @@ class RegisterActivity : AppCompatActivity() {
                                 val currentUserDb = nelsDatabaseReference!!.child(userId)
                                 currentUserDb.child("Nombre").setValue(firstName)
                                 currentUserDb.child("Apellidos").setValue(lastName)
-                                currentUserDb.child("Email").setValue(lastName)
-                                currentUserDb.child("Contraseña").setValue(lastName)
+                                currentUserDb.child("Email").setValue(email)
+                                currentUserDb.child("Contraseña").setValue(password)
                                 updateUserInfoAndUI()
+
+                                register_progressBar.visibility=ProgressBar.INVISIBLE
 
                             } else {
 
                                 Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                                Toast.makeText(this, "Fallo de autonticación, intentalo más tarde.", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(this, "Fallo de autenticación, intentalo más tarde.", Toast.LENGTH_SHORT).show()
+
+                                register_progressBar.visibility=ProgressBar.INVISIBLE
 
                             }
 
